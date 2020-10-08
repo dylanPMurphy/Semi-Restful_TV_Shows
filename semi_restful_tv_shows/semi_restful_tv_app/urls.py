58d7d46934f4c403f45b39d7961e701f80c515e1
dylanPMurphy <dylanmurphydba@gmail.com>
2020-10-07 17:17:10 -0700

    created all routes

--- semi_restful_tv_shows/semi_restful_tv_app/urls.py
+++ semi_restful_tv_shows/semi_restful_tv_app/urls.py
@@ -2,5 +2,11 @@ import path
 from . import views
 urlpatterns = [
     path('', views.index),
-
+    path('shows', views.home),
+    path('shows/new', views.newShow),
+    path('shows/create', views.createShow)
+    path('shows/<int: id>', views.readShow),
+    path('shows/<int: id>/edit', views.editShow),
+    path('shows/<int: id>/edit', views.updateShow),
+    path('shows/<int: id>/edit', views.deleteShow)
 ]
--- semi_restful_tv_shows/semi_restful_tv_app/views.py
+++ semi_restful_tv_shows/semi_restful_tv_app/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def index(request):
+    redirect(request, '/shows')
+
+
+def home(request):
+    pass
+
+def newShow(request):
+    pass
+def createShow(request):
+    pass
+
+def readShow(request):
+    pass
+
+
+def editShow(request):
+    pass
+def updateShow(request):
+    pass
+
+
+def deleteShow(request):
+    pass
